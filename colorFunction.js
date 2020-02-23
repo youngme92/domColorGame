7faf1c88cbb2b915b356ace7bf7f40b306211e72
youngme92 <youngme992@gmail.com>
2020-02-23 20:28:43 +0900

    bugFix

--- colorFunction.js
+++ colorFunction.js
@@ -3,6 +3,7 @@ var num = {};
 var squares = document.querySelectorAll(".squares");
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#messageDisplay");
+var navDisplay = document.querySelector("#navDisplay");
 // var num1 = 6;
 // var num2 = Math.floor(Math.random()*(num));
 // 버튼 변수설정
@@ -10,8 +11,10 @@ var newColor = document.querySelector("#newColor");
 var easy = document.querySelector("#easy");
 var hard = document.querySelector("#hard");
 var secondRow = document.querySelector("#secondRow");
+// 숫자 변수
 num.num1 = 6;
 num.num2 = Math.floor(Math.random()*(num.num1));
+hard.classList.add("selected");
 
 function colorInit(){
     for(i = 0; i < num.num1; i++){
@@ -40,6 +43,7 @@ function squareClick(){
         if(clickColor === pickColor){
             messageDisplay.innerHTML = "GOOD!";
             newColor.innerHTML = "Play again?"
+            navDisplay.style.backgroundColor = pickColor;
             for(i = 0; i < squares.length; i++){
                 squares[i].style.backgroundColor = pickColor;
             }
@@ -58,6 +62,8 @@ function easyBtnClick(){
     num.num2 = Math.floor(Math.random()*(num.num1));
     reset();
     secondRow.classList.add("hide");
+    easy.classList.add("selected");
+    hard.classList.remove("selected");
 }
 
 function hardBtn(){
@@ -69,19 +75,21 @@ function hardBtnClick(){
     num.num2 = Math.floor(Math.random()*(num.num1));
     reset();
     secondRow.classList.remove("hide");
+    hard.classList.add("selected");
+    easy.classList.remove("selected");
 }
-
-
 
 function newColorBtn(){
     newColor.addEventListener("click", reset);
 }
 // 리셋함수
 function reset(){
-    colorInit();
-    squaresInit();
+    num.num2 = Math.floor(Math.random()*(num.num1));
     messageDisplay.innerHTML = "";
     newColor.innerHTML = "new color";
+    navDisplay.style.backgroundColor = "steelblue";
+    colorInit();
+    squaresInit();
 }
 
 // 실행부분
